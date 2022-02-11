fd24a1fb358d7c90c5bfc01f0e057f0fa516db59
rafaelcardamoni <48846641+rafaelcardamoni@users.noreply.github.com>
2022-02-11 07:36:38 -0300

    added scrollreveal library to the project

---
+++ app.js
@@ -0,0 +1,4 @@
+ScrollReveal({ reset: true }).reveal('#hero', { delay: 200 });
+ScrollReveal({ reset: true }).reveal('#description', { delay: 200 });
+ScrollReveal({ reset: true }).reveal('#promotions', { delay: 200 });
+ScrollReveal({ reset: true }).reveal('#app', { delay: 200 });
