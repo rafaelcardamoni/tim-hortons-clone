22b7c33a1ad1a1da7afb9e7e3ecf307dfec9d7a1
rafaelcardamoni <48846641+rafaelcardamoni@users.noreply.github.com>
2022-02-11 07:56:08 -0300

    added direction and distance to scrollreveal animations

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-ScrollReveal({ reset: true }).reveal('#hero', { delay: 200 });
-ScrollReveal({ reset: true }).reveal('#description', { delay: 200 });
-ScrollReveal({ reset: true }).reveal('#promotions', { delay: 200 });
-ScrollReveal({ reset: true }).reveal('#app', { delay: 200 });
+ScrollReveal({ reset: true }).reveal('#hero', {
+  delay: 200,
+  origin: 'top',
+  distance: '50px'
+});
+ScrollReveal({ reset: true }).reveal('#description', {
+  delay: 200,
+  origin: 'right',
+  distance: '50px'
+});
+ScrollReveal({ reset: true }).reveal('#promotions', {
+  delay: 200,
+  origin: 'bottom',
+  distance: '50px'
+});
+ScrollReveal({ reset: true }).reveal('#app', {
+  delay: 200,
+  origin: 'left',
+  distance: '50px'
+});
